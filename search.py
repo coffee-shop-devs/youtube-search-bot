7377dce3529efdf7687c4435c37c8542d74f7c0e
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-11 23:24:02 -0600

    Update spacing on search.py

--- search.py
+++ search.py
@@ -17,9 +17,9 @@ from googleapiclient.errors import HttpError
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = 'REPLACE_ME'
+DEVELOPER_KEY            = 'REPLACE_ME'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
-YOUTUBE_API_VERSION = 'v3'
+YOUTUBE_API_VERSION      = 'v3'
 
 def youtube_search(options):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
